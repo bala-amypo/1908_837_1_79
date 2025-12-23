1f4c3da94ec774884979fc6ba76cde8cab9baec2
bala-amypo <bala@amypo.in>
2025-12-23 08:11:49 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenAPIConfig.java
+++ src/main/java/com/example/demo/config/OpenAPIConfig.java
@@ -1,16 +1,16 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Swagger;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI customSwagger() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
